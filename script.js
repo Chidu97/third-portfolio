1b96b141f12c142ed59e7f129d6b50df3f2ba53e
Chidu Ugochukwu <chidu.ugo@hotmail.com>
2023-03-17 00:09:51 +0100

    finished personal portfolio

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+// Linking to the menu-icon & navlist
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+// toggling the menu-icons open
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+// removing the menu list once the icon has been clicked
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+
+
+
+
+
